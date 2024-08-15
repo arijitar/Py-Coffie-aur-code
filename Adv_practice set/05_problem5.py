9922638fe868bf424c4f570549ff1f10af3a813d
Arijit roy <arijit.roy155k@gmail.com>
2024-08-15 15:51:30 +0000

    f string add

--- Adv_practice set/05_problem5.py
+++ Adv_practice set/05_problem5.py
@@ -6,4 +6,4 @@ n =int(input("Enter your number: "))
 table =[n*i for i in range(1, 11) ]
 
 with open("Tables.txt", "a") as f:
-    f.write(str(table) + "\n")
+    f.write(f"Table of {n}: {str(table)} \n")
